cc6f37372b368de16080444a65ec11905bb7bd0e
Hyph <79437385+Hyphrio@users.noreply.github.com>
2024-07-30 21:00:25 +0800

    Add feature

--- src/hello/install.sh
+++ src/hello/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+# MIT
+# Some lines of code adopted from the desktop-lite feature. (C) Microsoft Corporation. Licensed under MIT.
+
 set -e
 
-echo "Activating feature 'hello'"
-
-GREETING=${GREETING:-undefined}
-echo "The provided greeting is: $GREETING"
+echo "Activating feature 'WebView'"
 
 # The 'install.sh' entrypoint script is always executed as the root user.
 #
@@ -12,18 +12,33 @@ echo "The provided greeting is: $GREETING"
 # These may be useful in instances where the context of the final 
 # remoteUser or containerUser is useful.
 # For more details, see https://containers.dev/implementors/features#user-env-var
-echo "The effective dev container remoteUser is '$_REMOTE_USER'"
-echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
 
-echo "The effective dev container containerUser is '$_CONTAINER_USER'"
-echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+packages="
+  libwebkit2gtk-4.1-dev \
+  build-essential \
+  libxdo-dev \
+  libssl-dev \
+  libayatana-appindicator3-dev \
+  librsvg2-dev
+"
 
-cat > /usr/local/bin/hello \
-<< EOF
-#!/bin/sh
-RED='\033[0;91m'
-NC='\033[0m' # No Color
-echo "\${RED}${GREETING}, \$(whoami)!\${NC}"
-EOF
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
 
-chmod +x /usr/local/bin/hello
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+check_packages ${packages}
